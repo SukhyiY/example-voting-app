90a1bd32f949efe65a02ea251f9abfd489dd091c
Yaroslav Sukhyi <47447416+SukhyiY@users.noreply.github.com>
2019-10-15 12:07:08 +0300

    Update app.py

--- vote/app.py
+++ vote/app.py
@@ -7,13 +7,14 @@ import json
 
 option_a = os.getenv('OPTION_A', "Cats")
 option_b = os.getenv('OPTION_B', "Dogs")
+redis_password = os.getenv('REDIS_PASSWORD', "Dogs")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
 
 def get_redis():
     if not hasattr(g, 'redis'):
-        g.redis = Redis(host="redis", db=0, socket_timeout=5)
+        g.redis = Redis(host="redis", db=0, password=redis_password, socket_timeout=5)
     return g.redis
 
 @app.route("/", methods=['POST','GET'])
@@ -34,6 +35,7 @@ def hello():
         'index.html',
         option_a=option_a,
         option_b=option_b,
+        redis_password=redis_password,
         hostname=hostname,
         vote=vote,
     ))
